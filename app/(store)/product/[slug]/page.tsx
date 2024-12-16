aff6f0dc31c3dcf7d94536f82f3a654ea3bf0b03
Josphat Muriu <Muriujosphatt@gmail.com>
2024-12-16 20:47:07 +0300

    Directory Changes

---
+++ app/(store)/product/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
+import { notFound } from "next/navigation";
+
+async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+
+  if (!product) {
+    return notFound();
+  }
+  return <div>Product Page</div>;
+}
+
+export default ProductPage;
