02ac880424514e3c9ac6e5ce1aaca8ffec12bd8a
Josphat Muriu <Muriujosphatt@gmail.com>
2024-12-17 17:08:04 +0300

    structure error fixing.. Accidentally copied Layout from app directory to store

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,14 +1,20 @@
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { notFound } from "next/navigation";
 
-async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
+async function ProductPage({
+  params,
+}: {
+  params: Promise<{
+    slug: string;
+  }>;
+}) {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
 
   if (!product) {
     return notFound();
   }
-  return <div>Product Page</div>;
+  return <div className="container mx-auto px-4 py-8">Product Page</div>;
 }
 
 export default ProductPage;
