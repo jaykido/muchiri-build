0f89bc145937d6dd5565735d7d13762f20599554
Josphat Muriu <Muriujosphatt@gmail.com>
2024-12-18 17:45:34 +0300

    Major Bugg Fixed!

--- sanity/lib/products/getProductBySlug.ts
+++ sanity/lib/products/getProductBySlug.ts
@@ -3,22 +3,22 @@ import { sanityFetch } from "../live";
 
 export const getProductBySlug = async (slug: string) => {
   const PRODUCT_BY_ID_QUERY = defineQuery(`
-      
-        *[
-            _type == "product" && slug.current == $slug
-        ] | order(name asc) [0]
-        `);
+    *[
+        _type == "product" && slug.current == $slug
+    ] | order(name asc) [0]
+  `);
 
   try {
     const product = await sanityFetch({
       query: PRODUCT_BY_ID_QUERY,
-      params: {
-        slug,
-      },
+      params: { slug },
     });
+
+    console.log("Sanity Response:", product); // Log the result
 
     return product.data || null;
   } catch (error) {
     console.log("Error fetching product by ID:", error);
+    return null; // Ensure the error handling is correct
   }
 };
